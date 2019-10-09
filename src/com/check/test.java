18e04c3796d80562bfb98c937fafe0b07da204a0
YuvasTheBat <yuvasviki89@gmail.com>
2019-10-09 23:56:53 +0530

    addedMethod

--- src/com/check/test.java
+++ src/com/check/test.java
@@ -2,8 +2,14 @@ package com.check;
 
 public class test {
 	
+	public void printme() {
+		System.out.println("Printing from method");
+	}
 	public static void main(String args[]) {
 		System.out.println("From test class");
+		test tt = new test();
+		tt.printme();
+				
 	}
 
 }
