cb96e6facbb7089f11cad09ef771edcf0a39b84f
YuvasTheBat <yuvasviki89@gmail.com>
2019-12-23 22:25:11 +0530

    First Commit

--- src/com/check/test.java
+++ src/com/check/test.java
@@ -4,6 +4,7 @@ public class test {
 	
 	public void printme() {
 		System.out.println("Printing from method");
+		System.out.println("New print");
 	}
 	public static void main(String args[]) {
 		System.out.println("From test class");
